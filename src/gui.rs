3361f6adef147fd7e0722dea51c59f3f6c307b3e
Ozaren <krishna.sd.2012@gmail.com>
2018-05-09 16:53:23 -0400

    updated App cleaned up how rendering is handled

--- src/gui.rs
+++ src/gui.rs
@@ -1,17 +1,18 @@
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow as Window;
+use opengl_graphics::GlGraphics;
 
 pub trait App {
-    fn render(&self, args: &RenderArgs);
+    fn render(&self, args: &RenderArgs, mut gl: GlGraphics) -> GlGraphics;
     fn update(&mut self, args: &UpdateArgs);
 }
 
-pub fn start(window: &mut Window, app: &mut App) {
+pub fn start(window: &mut Window, app: &mut App, mut g: GlGraphics) {
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(window) {
         if let Some(r) = e.render_args() {
-            app.render(&r);
+            g = app.render(&r, g);
         }
 
         if let Some(u) = e.update_args() {
