d3f1d5b9c9e2e5744c8d69a8432d1b18ff0b723a
Ozaren <krishna.sd.2012@gmail.com>
2018-05-10 14:52:32 -0400

    added movement to pieces and capture conditions

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,16 +1,22 @@
 use piston::input::*;
 use opengl_graphics::{ GlGraphics };
-use std::cmp::{min, max};
 
 pub mod pieces;
 
 use super::gui::{App, Data};
-use self::pieces::{Piece, Alliance, Move};
+use self::pieces::{Piece, Alliance, MoveDir, Move};
 
 const BACKGROUND: [f32; 4] = [0.2, 0.35, 0.5, 1.0];
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
-const SELECTED: [f32; 4] = [0.7, 0.5, 0.3, 1.0];
+const SELECTED: [f32; 4] = [0.7, 0.7, 0.1, 1.0];
+const SELECTED_MOVE: [f32; 4] = [0.4, 0.4, 0.0, 1.0];
+const TEAMS: [[f32; 4]; 2] = 
+    [[183.0 / 255.0, 76.0  / 255.0, 55.0  / 255.0, 1.0]
+    ,[72.0  / 255.0, 179.0 / 255.0, 200.0 / 255.0, 1.0]];
+const TEAMS_DIM: [[f32; 4]; 2] = 
+    [[183.0 / 255.0, 76.0  / 255.0, 55.0  / 255.0, 0.5]
+    ,[72.0  / 255.0, 179.0 / 255.0, 200.0 / 255.0, 0.5]];
 
 pub struct Game {
     width: u32, height: u32,
@@ -21,8 +27,8 @@ pub struct Game {
 
 impl Game {
     pub fn new(width: u32, height: u32) -> Game {
-        let team_one: Alliance = Alliance::new("Team 1", [138.0 / 255.0, 219.0 / 255.0, 114.0 / 255.0, 1.0]);
-        let team_two: Alliance = Alliance::new("Team 2", [117.0 / 255.0, 36.0  / 255.0, 141.0 / 255.0, 1.0]);
+        let team_one: Alliance = Alliance::new("Team 1", TEAMS[0]);
+        let team_two: Alliance = Alliance::new("Team 2", TEAMS[1]);
 
         Game::from_vec(width, height, Vec::new(), vec![team_one, team_two])
     }
@@ -39,8 +45,8 @@ impl Game {
         self.turn = (self.turn + 1) % self.teams.len();
     }
 
-    fn cur_team(&self) -> &Alliance {
-        &self.teams[self.turn]
+    fn cur_team(&self) -> Alliance {
+        self.teams[self.turn].clone()
     }
 
     fn place(&mut self, x: u32, y: u32, team: usize) -> Result<(), String> {
@@ -66,6 +72,30 @@ impl Game {
         None
     }
 
+    fn get_piece_mut(&mut self, x: u32, y: u32) -> Option<&mut Piece> {
+        let (x, y) = (x as i32, y as i32);
+
+        for p in &mut self.pieces {
+            if p.x() == x && p.y() == y {
+                return Some(p);
+            }
+        }
+
+        None
+    }
+
+    fn remove_piece(&mut self, x: u32, y: u32) -> Result<(), String> {
+        let pos = self.pieces.iter().position(|p| p.x() == x as i32 && p.y() == y as i32);
+        match pos {
+            Some(index) => {
+                self.pieces.remove(index);
+                Ok(())
+            },
+            None =>
+                Err(String::from("No piece at position"))
+        }
+    }
+
     fn get_tile_size(&self, data: &Data) -> (f64, f64, f64) {
         let (s1, s2) = (data.screen_width as f64 / self.width as f64, data.screen_height as f64 / self.height as f64);
 
@@ -88,6 +118,23 @@ impl Game {
 
         ((x / s) as u32, (y / s) as u32)
     }
+
+    fn can_move(&self, piece: Option<&Piece>, x: u32, y: u32) -> bool {
+        let (ix, iy) = (x as i32, y as i32);
+
+        match piece {
+            None => false,
+            Some(p) => {
+                if !p.can_move(MoveDir::new(ix - p.x(), iy - p.y())) {
+                    false
+                } else if let Some(other) = self.get_piece(x, y) {
+                    other.team() != p.team()
+                } else {
+                    true
+                }
+            }
+        }
+    }
 }
 
 impl App for Game {
@@ -98,16 +145,21 @@ impl App for Game {
 
         gl.draw(args.viewport(), |c, g| {
             // Clear the screen.
-            clear(BACKGROUND, g);
+            clear(WHITE, g);
             let transform = c.transform.trans(dw, dh).scale(s, s); //.trans((data.screen_width as f64 - sz), (data.screen_height as f64 - sz) / 2.0);
+            let piece = self.get_piece(self.selected_x, self.selected_y);
+            
             for i in 0..self.width {
                 for j in 0..self.height {
                     let c = {
                         if i == self.selected_x && j == self.selected_y {
                             SELECTED
                         }
+                        else if self.can_move(piece, i, j) {
+                            SELECTED_MOVE
+                        }
                         else if (i + j) % 2 == 0 {
-                            WHITE
+                            TEAMS_DIM[self.turn]
                         } else {
                             BLACK
                         }
@@ -119,7 +171,7 @@ impl App for Game {
             }
 
             for p in &self.pieces {
-                let sq = rectangle::square(p.x() as f64 + 0.05, p.y() as f64 + 0.05, 0.9);
+                let sq = rectangle::square(p.x() as f64 + 0.3, p.y() as f64 + 0.3, 0.4);
                 ellipse(p.team().color, sq, transform, g);
             }
         });
@@ -131,20 +183,68 @@ impl App for Game {
 
     fn handle_mouse(&mut self, mouse_button: MouseButton, data: &Data) {
         let (x, y) = self.to_grid(data.mouse_x, data.mouse_y, data);
+        let (sx, sy) = (self.selected_x, self.selected_y);
+        let (mut do_remove, mut deselect) = (None, false);
 
         match mouse_button {
             MouseButton::Left => {
-                if self.selected_x == x && self.selected_y == y {
+                if sx == x && sy == y {
                     let turn = self.turn;
                     match self.place(x, y, turn) {
-                        Ok(()) => (),
-                        Err(msg) => println!("{}", msg)
+                        Ok(()) => 
+                            deselect = true,
+                        Err(msg) => {
+                            println!("{}", msg);
+                            println!("{:?}", self.get_piece(x, y));
+                        }
                     };
-                    self.inc();
+                } else {
+                    let dir = MoveDir::new(x as i32 - sx as i32, y as i32 - sy as i32);
+                    let cur_team = self.cur_team();
+
+                    if self.can_move(self.get_piece(sx, sy), x, y) {
+                        match self.get_piece(sx, sy) {
+                            None => (),
+                            Some(piece1) => {
+                                if *piece1.team() == cur_team {
+                                    match self.get_piece(x, y) {
+                                        None => (),
+                                        Some(piece2) => {
+                                            do_remove = Some(piece1.team() != piece2.team());
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        if let Some(b) = do_remove {
+                            if b {
+                                if let Err(msg) = self.remove_piece(x, y) {
+                                    println!("error = {}", msg);
+                                }
+                            }
+                        }
+
+                        match self.get_piece_mut(sx, sy) {
+                            Some(p) => {
+                                if (*p.team() == cur_team) && (do_remove.is_none() || do_remove.unwrap()) {
+                                    p.apply(Move::from(dir));
+                                    deselect = true;
+                                }
+                            },
+                            None => ()
+                        }
+                    }
                 }
 
-                self.selected_x = x;
-                self.selected_y = y;
+                if deselect {
+                    self.selected_x = 100;
+                    self.selected_y = 100;
+                    self.inc();
+                } else {
+                    self.selected_x = x;
+                    self.selected_y = y;
+                }
             },
             _ => {
                 self.selected_x = 100;
--- src/game/pieces.rs
+++ src/game/pieces.rs
@@ -1,3 +1,5 @@
+use std::ops::Shl;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Alliance {
     pub color: [f32; 4],
@@ -5,26 +7,23 @@ pub struct Alliance {
 }
 
 impl Alliance {
-    pub fn new(name: &str, color: [f32; 4]) -> Alliance {
-        Alliance { name: String::from(name), color }
+    pub fn new(name: &str, color: [f32; 4]) -> Self {
+        Self { name: String::from(name), color }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct Piece {
     x: i32, y: i32, moved: bool,
-    team: Alliance
-}
-
-pub struct Move {
-    dx: i32, dy: i32
+    team: Alliance, poss_moves: MoveDirSet
 }
 
 impl Piece {
-    pub fn new(x: u16, y: u16, team: &Alliance) -> Piece {
-        Piece {
+    pub fn new(x: u16, y: u16, team: &Alliance) -> Self {
+        Self {
             x: x as i32, y: y as i32,
-            moved: false, team: team.clone()
+            moved: false, team: team.clone(),
+            poss_moves: MoveDirSet::from(vec![(1,0), (-1,0), (0,1), (0,-1)])
         }
     }
 
@@ -45,11 +44,108 @@ impl Piece {
     pub fn team(&self) -> &Alliance {
         &self.team
     }
+
+    pub fn can_move(&self, dir: MoveDir) -> bool {
+        self.poss_moves.clone().into_iter().position(|pm| pm == dir).is_some()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct MoveDir {
+    dx: i32, dy: i32
+}
+
+impl MoveDir {
+    pub fn new(dx: i32, dy: i32) -> Self {
+        Self {
+            dx, dy
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MoveDirSet {
+    moves: Vec<MoveDir>
+}
+
+impl MoveDirSet {
+    pub fn new() -> Self {
+        Self {
+            moves: vec![]
+        }
+    }
+
+    pub fn from(del: Vec<(i32, i32)>) -> Self {
+        let mut moves = Vec::new();
+
+        for (dx, dy) in del {
+            moves.push(MoveDir::new(dx, dy));
+        }
+        
+        Self {
+            moves
+        }
+    }
+}
+
+impl IntoIterator for MoveDirSet {
+    type Item = MoveDir;
+    type IntoIter = MoveDirSetIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        MoveDirSetIterator::new(self)
+    }
+
+}
+
+impl Shl<MoveDir> for MoveDirSet {
+    type Output = MoveDirSet;
+
+    fn shl(mut self, rhs: MoveDir) -> Self::Output {
+        self.moves.push(rhs);
+
+        self
+    }
+}
+
+pub struct MoveDirSetIterator {
+    set: MoveDirSet,
+    cur: usize
+}
+
+impl MoveDirSetIterator {
+    fn new(set: MoveDirSet) -> Self {
+        Self {
+            set, cur: 0
+        }
+    }
+}
+
+impl Iterator for MoveDirSetIterator {
+    type Item = MoveDir;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur < self.set.moves.len() {
+            let val = self.set.moves[self.cur];
+        
+            self.cur += 1;
+
+            Some(val)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Move {
+    dx: i32, dy: i32
 }
 
 impl Move {
-    pub fn new(dx: i32, dy: i32) -> Move {
-        Move {dx, dy}
+    pub fn from(move_dir: MoveDir) -> Self {
+        Self {
+            dx: move_dir.dx, dy: move_dir.dy
+        }
     }
 }
 
